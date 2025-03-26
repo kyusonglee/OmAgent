545b7d3f8736bfad8a15c6592cb436a908e6178c
Kyusong Lee <kyusonglee@gmail.com>
2025-03-25 22:09:18 -0700

    update

--- examples/mcp_example/test_mcp.py
+++ examples/mcp_example/test_mcp.py
@@ -1,3 +1,4 @@
+os.environ["OMAGENT_MODE"] = "lite"
 from typing import List
 from pydantic import Field
 from omagent_core.models.llms.base import BaseLLMBackend
@@ -14,7 +15,6 @@ from omagent_core.utils.general import encode_image, read_image
 from omagent_core.tool_system.manager import ToolManager
 import asyncio
 
-os.environ["OMAGENT_MODE"] = "lite"
 # Set current working directory path
 CURRENT_PATH = Path(__file__).parents[0]
 
@@ -23,12 +23,6 @@ registry.import_module(project_path=CURRENT_PATH.joinpath("agent"))
 container.register_stm("SharedMemSTM")
 
 class LLMTest(BaseLLMBackend):
-    prompts: List[PromptTemplate] = Field(
-        default=[
-            PromptTemplate.from_template("You are a helpful assistant.", role="system"),
-            PromptTemplate.from_template("detect the objects in the image. the path is '{{image}}'. Return the result in json format. For examples, if the image is a fruit, the result should be like this: {'fruit': ['apple', 'banana', 'orange']}}", role="user"),
-        ]
-    )
     llm: OpenaiGPTLLM ={
         "name": "OpenaiGPTLLM", 
         "model_id": "gpt-4o-mini", 
@@ -42,7 +36,6 @@ class LLMTest(BaseLLMBackend):
         "llm": llm, 
         "tools": []
     }
-    #output_parser: DictParser = DictParser()
 
 llm_test = LLMTest(workflow_instance_id="temp")
 
